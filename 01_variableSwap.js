d6bc431990dc6a93aa02d91c9cab0b7bcce5b291
beingpushpak <beingpushpak@gmail.com>
2023-07-23 15:59:20 +0530

    Create 01_variableSwap.js

---
+++ 01_variableSwap.js
@@ -0,0 +1,9 @@
+//swpapping
+var num1=100;
+var num2=400;
+console.log("before Swap ","num1= ",num1 , "num2= ",num2);
+var temp = num1;
+console.log(temp ,num1 ,num2) ;
+num1=num2;
+num2=temp;
+console.log(num1, num2);
